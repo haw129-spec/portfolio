5c4f38c9e410beace12a7ba102b810a6ce30554b
haw129-spec <haw129@ucsd.edu>
2026-04-17 21:37:09 -0700

    Initial commit for lab 3

---
+++ global.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('IT’S ALIVE!');
+
+  document.body.insertAdjacentHTML(
+
+    'afterbegin',
+
+    `
+
+    <label class="color-scheme">
+
+      Theme:
+
+      <select>
+
+        <option value="light dark">Automatic</option>
+
+        <option value="light">Light</option>
+
+        <option value="dark">Dark</option>
+
+      </select>
+
+    </label>`
+
+  );
+
+  let select = document.querySelector('.color-scheme select');
+
+  if ("colorScheme" in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+  }
+
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value;
+  });
+
+
+
+
+  // 3.1.1 Define pages
+  const pages = [
+    { url: "", title: "Home" },
+    { url: "projects/", title: "Projects" },
+    { url: "contacts/", title: "Contact" },
+    { url: "resume/", title: "Resume" },
+    { url: "https://github.com/haw129-spec/portfolio", title: "Github" }
+  ];
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/portfolio-main/"
+    : "/portfolio/";
+
+  let nav = document.createElement("nav");
+  document.body.prepend(nav);
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    if (!url.startsWith('http')) {
+      url = BASE_PATH + url;
+    }
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    a.classList.toggle(
+      'current',
+      a.host === location.host && a.pathname === location.pathname,
+    );
+
+    if (a.host !== location.host) {
+      a.target = '_blank';
+    }
+
+
+  }
+
+  
+});
